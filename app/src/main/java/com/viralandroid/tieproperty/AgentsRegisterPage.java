7b4a0f0a57f55695ccb18cea485905194fb7d91f
Sravani <sravani@yellowsoft.in>
2017-05-22 13:58:46 +0530

    commiting all files

--- app/src/main/java/com/viralandroid/tieproperty/AgentsRegisterPage.java
+++ app/src/main/java/com/viralandroid/tieproperty/AgentsRegisterPage.java
@@ -1,9 +1,12 @@
 package com.viralandroid.tieproperty;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
@@ -11,18 +14,23 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
+
+import java.util.ArrayList;
 
 /**
  * Created by T on 18-05-2017.
  */
 
 public class AgentsRegisterPage extends Activity {
-    EditText name,email,phone,city_name,about;
-    TextView register_btn;
+    EditText name,email,phone,about;
+    TextView register_btn,city_name;
     ImageView close_btn;
+    ArrayList<Cities> citiesfrom_api;
+    String city_id;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -31,7 +39,7 @@ public class AgentsRegisterPage extends Activity {
         name = (EditText) findViewById(R.id.name);
         email = (EditText) findViewById(R.id.email);
         phone = (EditText) findViewById(R.id.phone);
-        city_name = (EditText) findViewById(R.id.city_name);
+        city_name = (TextView) findViewById(R.id.city_name);
         register_btn = (TextView) findViewById(R.id.register_btn);
         close_btn = (ImageView) findViewById(R.id.close_btn);
         about = (EditText) findViewById(R.id.about);
@@ -41,6 +49,16 @@ public class AgentsRegisterPage extends Activity {
                 finish();
             }
         });
+        citiesfrom_api = new ArrayList<>();
+
+        city_name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = onCreateDialogSingleChoice();
+                dialog.show();
+            }
+        });
+
         register_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -74,7 +92,7 @@ public class AgentsRegisterPage extends Activity {
                             .setBodyParameter("name",name_string)
                             .setBodyParameter("email",email_string)
                             .setBodyParameter("phone",phone_string)
-                            .setBodyParameter("city",city_string)
+                            .setBodyParameter("city",city_id)
                             .setBodyParameter("about",about_string)
                             .asJsonObject()
                             .setCallback(new FutureCallback<JsonObject>() {
@@ -84,8 +102,9 @@ public class AgentsRegisterPage extends Activity {
                                         progressDialog.dismiss();
                                     if (result.get("status").getAsString().equals("Success")){
                                         Toast.makeText(AgentsRegisterPage.this,result.get("message").getAsString(),Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(AgentsRegisterPage.this,LoginPage.class);
-                                        startActivity(intent);
+//                                        Intent intent = new Intent(AgentsRegisterPage.this,LoginPage.class);
+//                                        startActivity(intent);
+//                                        finish();
                                     }else {
                                         Toast.makeText(AgentsRegisterPage.this,result.get("message").getAsString(),Toast.LENGTH_SHORT).show();
                                     }
@@ -95,5 +114,68 @@ public class AgentsRegisterPage extends Activity {
             }
         });
 
+        get_cities();
+
+    }
+
+
+    public Dialog onCreateDialogSingleChoice() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        final CharSequence[] array = new CharSequence[citiesfrom_api.size()];
+        for(int i=0;i<citiesfrom_api.size();i++){
+
+            array[i] = citiesfrom_api.get(i).title;
+        }
+        builder.setTitle("Select City").setSingleChoiceItems(array, 0, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                String selectedItem = array[i].toString();
+                Log.e("select",selectedItem);
+                city_name.setText(selectedItem);
+                city_id = citiesfrom_api.get(i).id;
+
+            }
+        })
+
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+
+        return builder.create();
+    }
+
+
+    public void get_cities(){
+        Ion.with(this)
+                .load(Session.SERVER_URL+"cities.php")
+                .asJsonArray()
+                .setCallback(new FutureCallback<JsonArray>() {
+                    @Override
+                    public void onCompleted(Exception e, JsonArray result) {
+                        try {
+                            for (int i=0;i<result.size();i++){
+                                Log.e("response",result.get(i).toString());
+                                Cities cities = new Cities(result.get(i).getAsJsonObject(),AgentsRegisterPage.this);
+                                citiesfrom_api.add(cities);
+                            }
+
+                        }catch (Exception e1){
+                            e1.printStackTrace();
+                        }
+
+                    }
+                });
     }
 }
