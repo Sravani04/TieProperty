6d5217eb70f76ae1d5ded12ac44c8598b2b8484d
Sravani <sravani@yellowsoft.in>
2017-06-03 11:07:14 +0530

    commiting all files

--- app/src/main/java/com/viralandroid/tieproperty/TrendingPropertiesAdapter.java
+++ app/src/main/java/com/viralandroid/tieproperty/TrendingPropertiesAdapter.java
@@ -1,7 +1,6 @@
 package com.viralandroid.tieproperty;
 
 import android.content.Context;
-import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,11 +19,12 @@ import java.util.ArrayList;
  */
 
 public class TrendingPropertiesAdapter  extends PagerAdapter{
-     ArrayList<TrendingProperties> trendingProperties;
-     LayoutInflater inflater;
-     Context context;
-    MainActivity mainActivity;
+    Context context;
+    int images[];
+    LayoutInflater inflater;
     ArrayList<Properties> properties;
+    MainActivity mainActivity;
+    ArrayList<TrendingProperties> trendingProperties;
 
     public TrendingPropertiesAdapter(Context context,ArrayList<TrendingProperties> trendingProperties,ArrayList<Properties> properties,MainActivity mainActivity) {
         this.context = context;
@@ -35,37 +35,36 @@ public class TrendingPropertiesAdapter  extends PagerAdapter{
     }
 
     @Override
-    public void destroyItem(ViewGroup container, int position, Object object) {
-        container.removeView((View) object);
-    }
-
-    @Override
     public int getCount() {
-         return trendingProperties.size();
+        return trendingProperties.size();
     }
 
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((LinearLayout) object);
+    }
 
     @Override
-    public Object instantiateItem(ViewGroup view, final int position) {
-        View imageLayout = inflater.inflate(R.layout.trending_property_image, view, false);
+    public Object instantiateItem(ViewGroup container,final int position){
+        View itemView = inflater.inflate(R.layout.trending_property_image, container, false);
+        final ImageView imageView = (ImageView) itemView.findViewById(R.id.myImage);
+        LinearLayout trending_slide = (LinearLayout) itemView.findViewById(R.id.trending_slide);
+        final TextView property_name = (TextView) itemView.findViewById(R.id.property_name);
+//        imageView.setImageResource(images[position]);
+        try {
+            Ion.with(context)
+                    .load(trendingProperties.get(position).image)
+                    .withBitmap()
+                    .placeholder(R.drawable.placeholder500x250)
+                    .intoImageView(imageView);
+            container.addView(itemView);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
-        assert imageLayout != null;
-        final ImageView imageView = (ImageView) imageLayout
-                .findViewById(R.id.myImage);
-        final TextView property_name = (TextView) imageLayout.findViewById(R.id.property_name);
-        LinearLayout trending_slide = (LinearLayout) imageLayout.findViewById(R.id.trending_slide);
-
-
-
-        Ion.with(context).load(trendingProperties.get(position).image).withBitmap().placeholder(R.drawable.placeholder500x250).intoImageView(imageView);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent intent = new Intent(context,PropertyDetailPage.class);
-//                intent.putExtra("property_id",trendingProperties.get(position).id);
-//                intent.putExtra("trending_prop",trendingProperties.get(position));
-//                context.startActivity(intent);
-
                 mainActivity.get_single_property(trendingProperties.get(position).property_id);
                 Log.e("id",trendingProperties.get(position).property_id);
 
@@ -73,22 +72,12 @@ public class TrendingPropertiesAdapter  extends PagerAdapter{
             }
         });
         property_name.setText(trendingProperties.get(position).property_name);
-
-        view.addView(imageLayout, 0);
-        return imageLayout;
+        return itemView;
     }
 
-    @Override
-    public boolean isViewFromObject(View view, Object object) {
-        return view.equals(object);
-    }
 
     @Override
-    public void restoreState(Parcelable state, ClassLoader loader) {
-    }
-
-    @Override
-    public Parcelable saveState() {
-        return null;
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout) object);
     }
 }
